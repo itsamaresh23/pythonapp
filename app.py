b295880a726244ecdef60d9de821300740902351
Amaresh Mohapatra <95702804+itsamaresh23@users.noreply.github.com>
2022-07-11 12:35:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
-    return "Hello World!"
+    return "Hello World!, How are you?"
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
